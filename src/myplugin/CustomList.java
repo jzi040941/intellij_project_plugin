1bc37e389e4cec1581e95088d90ef2071153a9b6
jzi040941 <jzi040941@naver.com>
2017-01-04 05:34:13 +0900

    using psimodifiderlistowner I made likewise inferface method that can detect specific modifer and do some act

--- src/myplugin/CustomList.java
+++ src/myplugin/CustomList.java
@@ -1,6 +1,13 @@
 package myplugin;
 
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiModifierList;
+import com.intellij.psi.PsiModifierListOwner;
+import com.intellij.psi.PsiTarget;
+import org.jetbrains.annotations.NotNull;
+
 import javax.swing.*;
+import java.util.ArrayList;
 
 /**
  * Created by 노성훈 on 2017-01-04.
@@ -8,12 +15,24 @@ import javax.swing.*;
 public class CustomList extends JList {
     private DefaultListModel staticdlm = new DefaultListModel();
     public CustomList(){
+        setSelectionMode(DefaultListSelectionModel.SINGLE_SELECTION);
         setCellRenderer(new CustomListRenderer());
 
     }
 
     public void MakeModel(DefaultListModel dlm){
         staticdlm = dlm;
-        setModel(staticdlm);
+        setModel(dlm);
+    }
+
+    public void check(String[] strs){
+        PsiModifierListOwner[] staticarrays =  (PsiModifierListOwner[]) staticdlm.toArray();
+        for(PsiModifierListOwner modiferlist : staticarrays) {
+            for (String str : strs) {
+                if (modiferlist.hasModifierProperty(str)) {
+
+                }
+            }
+        }
     }
 }
--- src/myplugin/mypluginform.java
+++ src/myplugin/mypluginform.java
@@ -1,22 +1,16 @@
 package myplugin;
 
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.SelectionChangeListener;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.psi.*;
-import com.intellij.psi.util.PsiUtil;
-import com.intellij.ui.components.JBList;
-import com.intellij.ui.components.JBPanel;
 import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import com.intellij.uiDesigner.core.GridConstraints;
-import com.sun.istack.internal.NotNull;
 
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import java.util.ArrayList;
 
 /**
@@ -35,11 +29,6 @@ public class mypluginform implements ToolWindowFactory{
     private JPanel Panel2_bottom;
     private JPanel Panel3_bottom;
     private JPanel Panel4_bottom;
-    private ToolWindow myToolWindow;
-    private PsiPackage mypsipackage;
-    private JToolBar Panel2_Toolbar;
-    private JToolBar Panel3_Toolbar;
-    private JToolBar Panel4_Toolbar;
 
 
     private void init_lists_scroll(JList list,JPanel panel){
@@ -50,23 +39,18 @@ public class mypluginform implements ToolWindowFactory{
         panel.add(scrollPane);
         scrollPane.setVisible(true);
 
-
-        /*
-        JToggleButton static_button= new JToggleButton("static");
-        Panel2_bottom.add(static_button,new GridConstraints());
-        */
     }
 
     private void init_toolbar(){
-        Panel2_Toolbar = new ToggleButtons_panel(list2);
+        JToolBar Panel2_Toolbar = new ToggleButtons_panel(list2);
         Panel2_bottom.add(Panel2_Toolbar);
-        Panel3_Toolbar = new ToggleButtons_panel(list3);
+        JToolBar Panel3_Toolbar = new ToggleButtons_panel(list3);
         Panel3_bottom.add(Panel3_Toolbar);
-        Panel4_Toolbar = new ToggleButtons_panel(list4);
+        JToolBar Panel4_Toolbar = new ToggleButtons_panel(list4);
         Panel4_bottom.add(Panel4_Toolbar);
     }
 
-    private void init_packageslists(Project project) {
+    private void init_packagelists(Project project) {
         PsiPackage pack = JavaPsiFacade.getInstance(project).findPackage("");
         PsiPackage[] psiPackages = pack.getSubPackages();
 
@@ -96,29 +80,35 @@ public class mypluginform implements ToolWindowFactory{
 
     private void make_method_list(Project project,PsiClass pcls){
         DefaultListModel dlm = new DefaultListModel();
-        PsiMethod[] methods = pcls.getMethods();
-        PsiField[] fields = pcls.getFields();
+        try {
+            PsiMethod[] methods = pcls.getMethods();
 
-        if(methods != null) {
             for (PsiMethod method : methods) {
                 dlm.addElement(method);
             }
             list3.MakeModel(dlm);
         }
-        DefaultListModel dlm2 = new DefaultListModel();
-        if(fields != null) {
+        catch (Exception e) {
+
+        }
+
+        try {
+            PsiField[] fields = pcls.getFields();
+            DefaultListModel dlm2 = new DefaultListModel();
             for (PsiField field : fields) {
                 dlm2.addElement(field);
             }
+            list4.MakeModel(dlm2);
         }
-        list4.MakeModel(dlm2);
+        catch (Exception e){
+
+        }
 
     }
 
     private void make_class_list(Project project,Object[] objs){
         ArrayList<PsiClass[]> classeslist = new ArrayList<PsiClass[]>();
         for(Object obj : objs) {
-            Class cls = obj.getClass();
             classeslist.add(JavaPsiFacade.getInstance(project).findPackage(((PsiPackage) obj).getName()).getClasses());
         }
 
@@ -135,13 +125,10 @@ public class mypluginform implements ToolWindowFactory{
 
     private void init_form_design(){
         Panel1.add(list1);
-        list2.setSelectionMode(DefaultListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        list1.setSelectionMode(DefaultListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
         Panel2.add(list2);
-        list2.setSelectionMode(DefaultListSelectionModel.SINGLE_SELECTION);
         Panel3.add(list3);
-        list2.setSelectionMode(DefaultListSelectionModel.SINGLE_SELECTION);
         Panel4.add(list4);
-        list2.setSelectionMode(DefaultListSelectionModel.SINGLE_SELECTION);
         init_lists_scroll(list1,Panel1);
         init_lists_scroll(list2,Panel2);
         init_lists_scroll(list3,Panel3);
@@ -154,19 +141,13 @@ public class mypluginform implements ToolWindowFactory{
         init_form_design();
         init_toolbar();
 
-
-        //init_lists_scroll(list2,myToolWindowContent);
         list_selection_eventlistener_set(project);
 
-        myToolWindow = toolWindow;
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
         Content content = contentFactory.createContent(myToolWindowContent, "", false);
         toolWindow.getContentManager().addContent(content);
 
-        this.init_packageslists(project);
-
-
-
+        this.init_packagelists(project);
     }
 
 
